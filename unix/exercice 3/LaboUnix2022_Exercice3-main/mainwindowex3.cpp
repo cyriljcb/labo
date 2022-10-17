b20a8f442be3ff3e35712553b646b9c0757932c1
cyriljcb <jacobcyril042@gmail.com>
2022-10-17 09:09:14 +0200

    :memo:push

--- unix/exercice 3/LaboUnix2022_Exercice3-main/mainwindowex3.cpp
+++ unix/exercice 3/LaboUnix2022_Exercice3-main/mainwindowex3.cpp
@@ -1,5 +1,13 @@
 #include "mainwindowex3.h"
 #include "ui_mainwindowex3.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 
 MainWindowEx3::MainWindowEx3(QWidget *parent):QMainWindow(parent),ui(new Ui::MainWindowEx3)
 {
@@ -138,17 +146,113 @@ void MainWindowEx3::on_pushButtonLancerRecherche_clicked()
 {
   fprintf(stderr,"Clic sur le bouton Lancer Recherche\n");
   // TO DO
-  int idFils1,idFils2,idFils3;
+  int idFils1,idFils2,idFils3,status,id;
+  char buf[10];
+  int fd;
+  fd = open("Trace.log", O_CREAT | O_APPEND | O_WRONLY, 0666);
+  if(fd == -1)
+  {
+    perror("erreur d'ouverture");
+  }
+  if(dup2(fd,2)==-1)
+  {
+    perror("erreur de duplication du descripteur");
+    exit(1);
+  }
+  
+  if(ui->checkBoxRecherche1->isChecked()&& getGroupe1() != NULL)
+  {
+      if((idFils1= fork() ) == 0)
+      {
+        //processus fils1
+        
+        if(execlp("Lecture","Lecture",getGroupe1(),NULL) ==-1)
+        {
+          perror("erreur de execl()");
+          exit(1);
+        }
+    
+      }
+  }
+  if(ui->checkBoxRecherche2->isChecked()&& getGroupe2() != NULL)
+  {
+      if((idFils2= fork() ) == 0)
+      {
+        //processus fils2
+        
+        if(execlp("Lecture","Lecture",getGroupe2(),NULL) ==-1)
+        {
+          perror("erreur de execl()");
+          exit(1);
+        }
+    
+      }
+  }
+  if(ui->checkBoxRecherche3->isChecked()&& getGroupe3() != NULL)
+  {
+      if((idFils3= fork() ) == 0)
+      {
+        //processus fils3
+        
+        if(execlp("Lecture","Lecture",getGroupe3(),NULL) ==-1)
+        {
+          perror("erreur de execl()");
+          exit(1);
+        }
+    
+      }
+  }
+
+  //processus pere
+  while((id = wait(&status)) !=-1)
+  {
+    
+    if(WIFEXITED(status))
+    {
+      if(id ==idFils1)
+      {
+        sprintf(buf,"%d",WEXITSTATUS(status));
+        ui->lineEditResultat1->setText(buf);
+      }
+      else
+      {
+        if(id == idFils2)
+        {
+          sprintf(buf,"%d",WEXITSTATUS(status));
+          ui->lineEditResultat2->setText(buf);
+        }
+        else
+        {
+          if(id ==idFils3)
+          {
+            sprintf(buf,"%d",WEXITSTATUS(status));
+            ui->lineEditResultat3->setText(buf);
+          }
+        }
+      }
+
+    }
+  }
+
+  
+
+
 }
 
 void MainWindowEx3::on_pushButtonVider_clicked()
 {
   fprintf(stderr,"Clic sur le bouton Vider\n");
   // TO DO
+  ui->lineEditGroupe1->setText("");
+  ui->lineEditGroupe2->setText("");
+  ui->lineEditGroupe3->setText("");
+  ui->lineEditResultat1->setText("");
+  ui->lineEditResultat2->setText("");
+  ui->lineEditResultat3->setText("");
 }
 
 void MainWindowEx3::on_pushButtonQuitter_clicked()
 {
   fprintf(stderr,"Clic sur le bouton Quitter\n");
-  exit(1);
+  exit(0);
 }
